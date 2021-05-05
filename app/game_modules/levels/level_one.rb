4fe24ddf325c3f6d34914fd739e49e2b30fcc214
Tariq Mamdouh Ahmed <tarq.mamdouh1996@gmail.com>
2021-05-06 00:38:09 +0200

    LvlOneScene: Fixed English typos and increased timeout

--- app/game_modules/levels/level_one.rb
+++ app/game_modules/levels/level_one.rb
@@ -14,10 +14,10 @@ module GameModules
             # starting scene
             def play_starting_scene
                 # Story Starting Scenes
-                Scene.play('lvl01_01', 60, { name: 'Fadein', direction: 'right', story: true})
-                Scene.play('lvl01_02', 60, { name: 'Fadein', direction: 'bottom', story: true})
-                Scene.play('lvl01_03', 60, { name: 'Fadein', direction: 'top', story: true})
-                Scene.play('lvl01_04', 60, { name: 'Fadein', direction: 'bottom', story: true})
+                Scene.play('lvl01_01', 90, { name: 'Fadein', direction: 'right', story: true})
+                Scene.play('lvl01_02', 90, { name: 'Fadein', direction: 'bottom', story: true})
+                Scene.play('lvl01_03', 90, { name: 'Fadein', direction: 'top', story: true})
+                Scene.play('lvl01_04', 90, { name: 'Fadein', direction: 'bottom', story: true})
             end
         end
     end
