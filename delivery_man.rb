e05227b9a22fd7ed65e424884bffea537350b5c1
Tariq Mamdouh Ahmed <tarq.mamdouh1996@gmail.com>
2021-05-08 03:26:55 +0200

    Application: Added Window validation

--- delivery_man.rb
+++ delivery_man.rb
@@ -8,4 +8,18 @@ require 'tty-prompt'
 require_all 'app'
 require_all 'lib'
 
-GameModules::Main.main
+# Validate the game can run in your console
+loop do
+    if ApplicationWindow.game_fits_console
+        break
+    else
+        system("clear")
+        puts "your console size is not enough to run the game, 
+              please increase it...\n
+              current: #{ApplicationWindow.width} X #{ApplicationWindow.height} \n
+              Required: #{ApplicationWindow.min_width} X #{ApplicationWindow.min_height} \n
+              CTRL + C to Exit"
+        sleep(0.5)
+    end
+end
+GameModules::Main.start
